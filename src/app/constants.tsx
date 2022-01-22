29fbdb9d665bff2747f82494c91aa07ca7524aec
nutchanon.ust <nutchanon.pho@gmail.com>
2022-01-22 21:13:40 +0700

    feat: execute place bid success

--- src/app/constants.tsx
+++ src/app/constants.tsx
@@ -1,2 +1,26 @@
 export const ANCHOR_LIQUIDATION_QUEUE_CONTRACT_ADDRESS =
   'terra1e25zllgag7j9xsun3me4stnye2pcg66234je3u';
+
+export const FALLBACK_GAS_PRICE_COLUMNBUS = {
+  uluna: '0.01133',
+  usdr: '0.104938',
+  uusd: '0.15',
+  ukrw: '169.77',
+  umnt: '428.571',
+  ueur: '0.125',
+  ucny: '0.98',
+  ujpy: '16.37',
+  ugbp: '0.11',
+  uinr: '10.88',
+  ucad: '0.19',
+  uchf: '0.14',
+  uaud: '0.19',
+  usgd: '0.2',
+  uthb: '4.62',
+  usek: '1.25',
+  unok: '1.25',
+  udkk: '0.9',
+  uidr: '2180.0',
+  uphp: '7.6',
+  uhkd: '1.17',
+};
--- src/app/pages/AppPage/NewBidForm/index.tsx
+++ src/app/pages/AppPage/NewBidForm/index.tsx
@@ -8,13 +8,23 @@ import styled from 'styled-components/macro';
 import { useTranslation } from 'react-i18next';
 import { messages } from './messages';
 import { Form, Input, Button, Checkbox, InputNumber } from 'antd';
-import { CreateTxOptions, MsgExecuteContract } from '@terra-money/terra.js';
+import {
+  CreateTxOptions,
+  Dec,
+  Fee,
+  LCDClient,
+  MsgExecuteContract,
+} from '@terra-money/terra.js';
 import {
   ConnectedWallet,
   useConnectedWallet,
+  useLCDClient,
 } from '@terra-money/wallet-provider';
-import { ANCHOR_LIQUIDATION_QUEUE_CONTRACT_ADDRESS } from 'app/constants';
-import { useCallback } from 'react';
+import {
+  ANCHOR_LIQUIDATION_QUEUE_CONTRACT_ADDRESS,
+  FALLBACK_GAS_PRICE_COLUMNBUS,
+} from 'app/constants';
+import { useMemo } from 'react';
 
 interface Props {}
 
@@ -22,15 +32,19 @@ export function NewBidForm(props: Props) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { t, i18n } = useTranslation();
   const connectedWallet = useConnectedWallet() as ConnectedWallet;
-  console.log('connectedWallet', connectedWallet);
+  const network = connectedWallet?.network;
+  const lcd = useMemo(() => {
+    if (!network) return;
+    return new LCDClient({
+      chainID: network.chainID,
+      URL: network.lcd,
+    });
+  }, [network?.chainID, network?.lcd]);
 
   const [form] = Form.useForm();
-  console.log('form', form);
 
   const postTx = async () => {
-    console.log('postTx');
-    console.log('connectedWallet', connectedWallet);
-    if (!connectedWallet) return;
+    if (!connectedWallet || !lcd) return;
     await form.validateFields();
     console.log(form.getFieldValue('premium'));
     const msg = new MsgExecuteContract(
@@ -44,11 +58,36 @@ export function NewBidForm(props: Props) {
       },
       { uusd: 10000 },
     );
+    const { auth_info } = await lcd.tx.create(
+      [{ address: connectedWallet?.walletAddress }],
+      {
+        msgs: [msg],
+        feeDenoms: ['uusd'],
+        gasPrices: FALLBACK_GAS_PRICE_COLUMNBUS,
+      },
+    );
+    const estimatedFeeGas = auth_info.fee.amount
+      .toArray()
+      .reduce((gas, coin) => {
+        console.log(coin.denom);
+        //@ts-ignore
+        const price = FALLBACK_GAS_PRICE_COLUMNBUS[coin.denom];
+        console.log(coin.amount.toString());
+        console.log(price);
+        return gas.add(coin.amount.div(price));
+      }, new Dec(0));
+    console.log('auth_info', auth_info);
+    console.log('estimatedFeeGas', estimatedFeeGas.toString());
     const tx: CreateTxOptions = {
       msgs: [msg],
+      fee: new Fee(estimatedFeeGas.toNumber(), auth_info.fee.amount),
     };
-    const result = await connectedWallet.post(tx);
-    console.log(result);
+    try {
+      const result = await connectedWallet.post(tx);
+      console.log(result);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
